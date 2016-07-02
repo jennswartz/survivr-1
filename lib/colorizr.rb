70fe8e42cfa8d13ca9849bd9698fc77fc871afca
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-02 16:14:07 -0400

    Added colors

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,9 +1,34 @@
 class String
-  def red(text)
-    "\e[31m#{text}\e[0m"
+  @colors = { red: 31, green: 32, yellow: 33, blue: 34, pink: 35, lightblue: 36,
+    white: 37, black: 30 }
+
+  def self.colors
+    @colors.keys
   end
 end
 
+def self.create_colors
+    @colors.each do |color, number|
+      send(:define_method, color) do
+        "\e[#{number}m#{self}\e[0m"
+      end
+    end
+  end
+
+  def self.sample_colors
+    @colors.each do |color, number|
+      puts "This is \e[#{number}m#{color}\e[0m"
+    end
+  end
+end
+
+#String.create_colors
+
+# class String
+#   def create_greeting
+#     self.prepend("Hello ")
+#   end
+# end
 # class String
 #   # Takes in a name argument and adds Hello to the front
 #   def create_greeting(name)
--- test.rb
+++ test.rb
@@ -1,10 +1,23 @@
 require 'colorizr'
 
-puts "\e[34mThis is blue text.\e[0m"
-puts "\e[35mThis is magenta text\e[0m"
-
+require 'colorizr'
 
 puts "John".red
+puts "Paul".green
+puts "George".blue
+puts "Ringo".yellow
+
+puts String.colors
+
+String.sample_colors
+
+# puts "\e[34mThis is blue text.\e[0m"
+# puts "\e[35mThis is magenta text\e[0m"
+# 
+# 
+# String.colors
+
+#puts "John".red
 # puts "Paul".green
 # puts "George".blue
 # puts "Ringo".yellow
